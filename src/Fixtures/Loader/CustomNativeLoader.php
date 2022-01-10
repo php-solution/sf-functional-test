351f49f872395bce25f59284fc42b5813f57deba
Oleksii Romanchenko <ol.romanchenko@foodtech.team>
2022-01-10 15:22:30 +0200

    Add UUID support

--- src/Fixtures/Loader/CustomNativeLoader.php
+++ src/Fixtures/Loader/CustomNativeLoader.php
@@ -8,6 +8,7 @@ use Faker\Factory as FakerGeneratorFactory;
 use Faker\Generator as FakerGenerator;
 use Nelmio\Alice\Faker\Provider\AliceProvider;
 use \Nelmio\Alice\Loader\NativeLoader;
+use PhpSolution\FunctionalTest\Fixtures\Faker\Provider\SymfonyUuidProvider;
 use PhpSolution\FunctionalTest\Fixtures\Faker\Provider\UUIDProvider;
 
 class CustomNativeLoader extends NativeLoader
@@ -17,6 +18,7 @@ class CustomNativeLoader extends NativeLoader
         $generator = FakerGeneratorFactory::create(static::LOCALE);
         $generator->addProvider(new AliceProvider());
         $generator->addProvider(new UUIDProvider());
+        $generator->addProvider(new SymfonyUuidProvider());
         $generator->seed($this->getSeed());
 
         return $generator;
