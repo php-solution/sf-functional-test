2ac5a81c32541feae211578c8116a7a19964f764
ypft <157708858+ypft@users.noreply.github.com>
2024-01-31 12:19:52 +0200

    [TECH] Get back ConsoleTestCase::runConsoleCommand for bc

--- src/TestCase/ConsoleTestCase.php
+++ src/TestCase/ConsoleTestCase.php
@@ -5,12 +5,27 @@ declare(strict_types=1);
 namespace PhpSolution\FunctionalTest\TestCase;
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 class ConsoleTestCase extends AppTestCase
 {
-    public static function runConsoleCommand(ArgvInput $input, Application $consoleApp = null, bool $autoExit = false): BufferedOutput
+    public static function runConsoleCommand(string $name, array $parameters = [], Application $consoleApp = null): BufferedOutput
+    {
+        $parameters['-e'] = $parameters['-e'] ?? 'test';
+
+        // By default, set output verbosity - verbose
+        if (0 === count(array_intersect(array_keys($parameters), ['-q', '-v', '-vv', '-vvv']))) {
+            $parameters['-v'] = true;
+        }
+        $parameters = array_merge(['command' => $name], $parameters);
+        $input = new ArrayInput($parameters);
+
+        return self::runCommand($input, $consoleApp);
+    }
+
+    public static function runCommand(InputInterface $input, Application $consoleApp = null, bool $autoExit = false): BufferedOutput
     {
         $consoleApp ??= self::createConsoleApp([], $autoExit);
 
